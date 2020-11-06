db092fdc0b47c8c1dd8c69f7387812eda1c5b514
jackb462 <74027993+jackb462@users.noreply.github.com>
2020-11-05 19:29:42 -0500

    Add files via upload

---
+++ testphp.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+
+This is HTML part. <br>
+Hello world in HTML <br>
+<?php
+	echo "Hello World in PHP!<br>";
+?>
+Back to HTML again! <br>
+<?php
+	echo "Hello World in PHP again!";
+?>
+</body>
+</html>
